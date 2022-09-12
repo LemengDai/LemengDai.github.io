83073caf78a457f5de511d1b1da573b8328c49eb
LemengDai <51035918+LemengDai@users.noreply.github.com>
2022-09-11 22:35:38 -0400

    comment out typewriter effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,27 +58,28 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-window.onload = startFunction;
+// Typewriter
+// window.onload = startFunction;
 
-function startFunction() {
-  var txt = "Hello, I'm Lemeng Dai : )";
-  var i = 0;
-  var speed = 300;
-  typeWriter();
-  setInterval(cursorAnimation, 500);
+// function startFunction() {
+//   var txt = "Hello, I'm Lemeng Dai : )";
+//   var i = 0;
+//   var speed = 300;
+//   typeWriter();
+//   setInterval(cursorAnimation, 500);
 
-  function typeWriter() {
-    if (i < txt.length) {
-      document.getElementById("typeWriter").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    }
-  }
-}
+//   function typeWriter() {
+//     if (i < txt.length) {
+//       document.getElementById("typeWriter").innerHTML += txt.charAt(i);
+//       i++;
+//       setTimeout(typeWriter, speed);
+//     }
+//   }
+// }
 
-function cursorAnimation() {
-  var cursor = document.getElementById("cursor");
-  cursor.animate({
-    opacity: [0, 1], 
-  },500)
-}
+// function cursorAnimation() {
+//   var cursor = document.getElementById("cursor");
+//   cursor.animate({
+//     opacity: [0, 1], 
+//   },500)
+// }
